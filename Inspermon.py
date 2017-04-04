ce02df4c40e821bb40be798a7682af6d50f5cba4
Bruno Costa <brunex887@gmail.com>
2017-04-04 16:45:05 -0300

    New Game Load Game - c bug

--- Inspermon.py
+++ Inspermon.py
@@ -15,7 +15,7 @@ insperdex={
 		"Ataque":120, "Defesa":15, "Vida":180, "Exp":35, "Chance":2, "Evo":10
 	},
 	"Douglas":{
-		"Ataque":90, "Defesa":25, "Vida":210, "Exp":30, "Chance":3
+		"Ataque":90, "Defesa":25, "Vida":210, "Exp":30, "Chance":3, 
 	},
 	"Cetaxu":{
 		"Ataque":70, "Defesa":50, "Vida":220, "Exp":30, "Chance":0
@@ -39,7 +39,7 @@ dexjog={} #InsperDex
 
 def save(arquivo):
 	dados= open(arquivo,'wb') 
-	pickle.dump({"dados" : [dexjog, e, jogador]}, dados)
+	pickle.dump({"dados" : [dexjog, exp, jogador]}, dados)
 	dados.close()
 
 
@@ -64,11 +64,14 @@ def luta(): #Função Fuga
 
 lista_chance=[] #Lista de chance de encontro no passeio
 for i in insperdex:
+	
 	if insperdex[i]["Chance"]>0:
 		x=0
+		
 		while x<insperdex[i]["Chance"]:
 			lista_chance.append(i)
 			x=x+1
+	
 	elif insperdex[i]["Chance"]<=0:
 		continue
 
@@ -99,66 +102,88 @@ def evolucao(jogador):
 
 
 def batalha(jogador,oponente): #Função Batalha
-	vidajog=insperdex[jogador]["Vida"]
+	vidajog=insperdex[jogador]["Vida"]+10*niveljog
 	vidaopo=insperdex[oponente]["Vida"]
 	print("Você encontrou um {} selvagem!".format(oponente))
 	time.sleep(1.0)
 	print("Os atributos do {} são: {}".format(oponente,insperdex[oponente]))
+	
 	if oponente not in dexjog:
 		dexjog[oponente]=insperdex[oponente]
 	time.sleep(1.5)
+	
 	while vidajog>0 and vidaopo>0:
 		acao=str(input("Qual o seu comando? (Lutar(L) ou Fugir(F))")).lower() #Escolha de comando
+		
 		if acao == "lutar" or acao == "l": 
 			time.sleep(1.0)
-			if (insperdex[jogador]["Ataque"]-insperdex[oponente]["Defesa"])>0:
+			
+			if ((insperdex[jogador]["Ataque"]+10*niveljog)-insperdex[oponente]["Defesa"])>0:
+				
 				if vidajog>0: #Ataque Jogador
+					
 					if critico()==True:
-						vidaopo=vidaopo-(insperdex[jogador]["Ataque"]-insperdex[oponente]["Defesa"])*1.5
+						vidaopo=vidaopo-((insperdex[jogador]["Ataque"]+10*niveljog)-insperdex[oponente]["Defesa"])*1.5
+						
 						if vidaopo>0:
 							print("O {} leva um ataque CRÍTICO e fica com {} de vida".format(oponente,vidaopo))
 							time.sleep(1.0)
+						
 						elif vidaopo<=0:
 							print("O {} leva um ataque CRÍTICO e desmaia!".format(oponente))
 							time.sleep(1.0)
+					
 					else:
-						vidaopo=vidaopo-(insperdex[jogador]["Ataque"]-insperdex[oponente]["Defesa"])
+						vidaopo=vidaopo-((insperdex[jogador]["Ataque"]+10*niveljog)-insperdex[oponente]["Defesa"])
+						
 						if vidaopo>0:
 							print("O {} leva o ataque e fica com {} de vida".format(oponente,vidaopo))
 							time.sleep(1.0)
+						
 						elif vidaopo<=0:
 							print("O {} leva o ataque e desmaia!".format(oponente))
 							time.sleep(1.0)
-			elif (insperdex[jogador]["Ataque"]-insperdex[oponente]["Defesa"])<=0:
+			
+			elif ((insperdex[jogador]["Ataque"]+10*niveljog)-insperdex[oponente]["Defesa"])<=0:
 				print("O seu ataque não deu dano!")
 			
-			if (insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])>0:
+			
+			if (insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))>0:
+				
 				if vidaopo>0: #Ataque Oponente
 					print("O {} selvagem se prepara para atacar!".format(oponente))
 					time.sleep(1)
+					
 					if critico()==True:
-						vidajog=vidajog-(insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])*1.5
+						vidajog=vidajog-(insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))*1.5
+						
 						if vidajog>0:
 							print("O seu {} é atacado CRITICAMENTE e fica com {} de vida".format(jogador,vidajog))
 							time.sleep(1.0)
+						
 						elif vidajog<=0:
 							print("O seu {} é atacado CRITICAMENTE e desmaia!".format(jogador))
 							time.sleep(1.0)
+					
 					else:
-						vidajog=vidajog-(insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])
+						vidajog=vidajog-(insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))
+						
 						if vidajog>0:
 							print("O seu {} é atacado e fica com {} de vida".format(jogador,vidajog))
 							time.sleep(1.0)
+						
 						elif vidajog<=0:
 							print("O seu {} é atacado e desmaia!".format(jogador))
 							time.sleep(1.0)
-			elif (insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])<=0:
+			
+			elif (insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))<=0:
 				print("O ataque do {} não deu dano!".format(oponente))
 			
 			if vidaopo<=0: #Resultado Batalha
 				print(("Você venceu! Seu {} ganhou {} de exp!").format(jogador,insperdex[oponente]["Exp"]))
 				time.sleep(1.0)
 				return True
+			
 			elif vidajog<=0:
 				print("Você perdeu e seu Inspèrmon foi levado ao InsperCenter!")
 				time.sleep(1.0)
@@ -166,52 +191,81 @@ def batalha(jogador,oponente): #Função Batalha
 		
 		if acao == "fugir" or acao == "f": #Tentativa de Fuga
 			time.sleep(1.0)
+			
 			if luta()==True:
 				print("Fuga Falhou!")
 				time.sleep(1.0)
+				
 				if critico()==True:
-					vidajog=vidajog-(insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])*1.5
+					vidajog=vidajog-(insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))*1.5
+					
 					if vidajog>0:
 						print("O seu {} é atacado CRITICAMENTE e fica com {} de vida".format(jogador,vidajog))
 						time.sleep(1.0)
+					
 					elif vidajog<=0:
 						print("O seu {} é atacado CRITICAMENTE e desmaia!".format(jogador))
 						time.sleep(1.0)
+				
 				else:
-					vidajog=vidajog-(insperdex[oponente]["Ataque"]-insperdex[jogador]["Defesa"])
+					vidajog=vidajog-(insperdex[oponente]["Ataque"]-(insperdex[jogador]["Defesa"]+10*niveljog))
+					
 					if vidajog>0:
 						print("O seu {} é atacado e fica com {} de vida".format(jogador,vidajog))
 						time.sleep(1.0)
+					
 					elif vidajog<=0:
 						print("O seu {} é atacado e desmaia!".format(jogador))
 						time.sleep(1.0)
+			
 			else:
 				print("Fuga Sucedida")
 				time.sleep(1.0)
 				break
 
 
-
-e=0
+niveljog=1
+exp=0
+lista_saves=[]
 print("Bem Vindo ao Mundo de Inspermon!")
 time.sleep(0.5)
+inicio=str(input("New Game(N) ou Load Game(L)? ")).lower()
+if inicio=="new game" or inicio=="n":
+	jogador=str(input("Qual seu Inspermon inicial? (Pikaxu, Kapuznakara, Xanaina, Kingnaldo ou Douglas) ")).title()
+	dexjog[jogador]=insperdex[jogador]
+	time.sleep(0.5)
+if inicio=="load game" or inicio=="l":
+	lista_saves=pickle.load(open("lista","rb"))
+			
+		if len(lista_saves)>0:
+			print("Os saves disponíveis são: {}".format(lista_saves))
+			time.sleep(1)
+			file=str(input("Qual save deseja carregar? "))
+			dado=load(file)
+			dexjog=dado["dados"][0]
+			exp=dado["dados"][1]
+			jogador=dado["dados"][2]
+			print("Carregando....")
+			time.sleep(2)
+			print("Sucesso!")
+			time.sleep(0.5)
+		if len(lista_saves)==0:
+			print("Não há saves ainda!")
 
-jogador=str(input("Qual seu Inspermon inicial? (Pikaxu, Kapuznakara, Xanaina, Kingnaldo ou Showglas) ")).title()
-dexjog[jogador]=insperdex[jogador]
-time.sleep(0.5)
-list
-lista_saves=[]
 while True:
 	fazer=str(input("O que você vai fazer? (Passear(P), Dormir(D), Insperdex(I), Salvar(S) ou Load(L)? ")).lower() #Escolha de ação
 	time.sleep(1.0)
+	
 	if fazer == "passear" or fazer == "p": #Caso Passeie
 		print("Passeando...")
 		time.sleep(1.5) 
 		oponente=roll()
+		
 		if batalha(jogador,oponente)==True:
-			e=experiencia(e)
-		print("Seu {} possui {} de experiência".format(jogador,e))
-		if e >=insperdex[jogador]["Evo"]:
+			exp=experiencia(exp)
+		print("Seu {} possui {} de experiência".format(jogador,exp))
+		
+		if exp >=insperdex[jogador]["Evo"]:
 			jogador=evolucao(jogador)
 			print("O seu pokemon evoluiu para {}! ".format(jogador))
 			dexjog[jogador]=insperdex[jogador]
@@ -220,10 +274,13 @@ while True:
 	if fazer == "dormir" or fazer == "d": #Caso durma
 		print("Bons Sonhos!")
 		break 
+	
 	if fazer == "insperdex" or fazer == "i": #Caso cheque Insperdex
 		print("Este são os Inspèrmons que você já encontrou: {}".format(dexjog))
 		time.sleep(0.5)
+	
 	if fazer == "salvar" or fazer == "s":
+		print("Os saves disponíveis são: {}".format(lista_saves))
 		arquivo=str(input("Deseja salvar com que nome? "))
 		lista_saves.append(arquivo)
 		save(arquivo)
@@ -234,20 +291,23 @@ while True:
 		time.sleep(2)
 		print("Sucesso!")
 		time.sleep(0.5)
+	
 	if fazer == "load" or fazer == "l":
 		lista_saves=pickle.load(open("lista","rb"))
+		
 		if len(lista_saves)>0:
 			print("Os saves disponíveis são: {}".format(lista_saves))
 			time.sleep(1)
 			file=str(input("Qual save deseja carregar? "))
 			dado=load(file)
 			dexjog=dado["dados"][0]
-			e=dado["dados"][1]
+			exp=dado["dados"][1]
 			jogador=dado["dados"][2]
 			print("Carregando....")
 			time.sleep(2)
 			print("Sucesso!")
 			time.sleep(0.5)
+		
 		if len(lista_saves)==0:
 			print("Não há saves ainda!")
 
